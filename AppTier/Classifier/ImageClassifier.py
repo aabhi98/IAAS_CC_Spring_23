71423cf09f9c3d334d50635a3fb434b4cd32eca4
aabhi98 <112668388+aabhi98@users.noreply.github.com>
2023-02-20 14:12:34 -0700

    update: 1

--- AppTier/Classifier/ImageClassifier.py
+++ AppTier/Classifier/ImageClassifier.py
@@ -20,11 +20,12 @@ class ImageClassifier:
         loop = True
         while loop:
             try:
+                print("ImageClassifier: entered")
                 message = self.aws_utils.receive_message_from_request_queue()
                 print(message['Body'])
                 image_data = base64.b64decode(message['Body'])
-                img = Image.open(BytesIO(image_data))
-                img.show()
+                #img = Image.open(BytesIO(image_data))
+                #img.show()
 
                 local_image_path = os.path.join(os.getcwd(), 'image.jpg')
                 with open(local_image_path, "wb") as f:
@@ -42,6 +43,7 @@ class ImageClassifier:
                 self.aws_utils.upload_to_response_s3(message['MessageId'], response_image_data)
                 self.aws_utils.send_message_to_response_queue(response_body)
                 self.aws_utils.delete_message_from_sqs(self.aws_utils.sqs, message['ReceiptHandle'])
+                print("ImageClassifier: exit")
 
             except Exception as e:
                 log.exception(f"An error occurred while processing the message: {e}")
@@ -49,6 +51,8 @@ class ImageClassifier:
 
     def get_result(self, image_path):
         try:
+            print("get result entered")
+            print(image_path)
             command = f"python image_classification.py {image_path}"
             result = subprocess.check_output(command, shell=True)
             return result.decode("utf-8")
