ed7569811fcae03f813c558ca262f1e7cc91c89a
aabhi98 <112668388+aabhi98@users.noreply.github.com>
2023-02-21 15:22:56 -0700

    image_classification.py change 'weight to pretrained'

--- AppTier/image_classification.py
+++ AppTier/image_classification.py
@@ -4,6 +4,7 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
+from urllib.request import urlopen
 from PIL import Image
 import numpy as np
 import json
@@ -14,7 +15,7 @@ url = str(sys.argv[1])
 #img = Image.open(urlopen(url))
 img = Image.open(url)
 
-model = models.resnet18(weights=True)
+model = models.resnet18(pretrained=True)
 
 model.eval()
 img_tensor = transforms.ToTensor()(img).unsqueeze_(0)
